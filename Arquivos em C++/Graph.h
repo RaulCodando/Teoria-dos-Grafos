126adb3fd99c4a954999618f172d864f8251ac5d
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-09-24 12:07:55 -0300

    Create Graph.h

---
+++ Arquivos em C++/Graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+
+class Graph{
+
+protected:
+
+    std::vector<int> visited;
+    bool isDirected;
+
+public:
+
+    Graph(int n, bool isDirected);
+
+    virtual void dfsRec(int v, int &cnt) = 0;
+
+    virtual void dfs() = 0;
+
+    virtual void bfs(int v0) = 0;
+};
+
+#endif //GRAPH_H
