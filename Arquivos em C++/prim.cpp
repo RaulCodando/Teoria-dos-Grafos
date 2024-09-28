582eafa05424ffb7efc4c48c8c0b42e9430602a6
RaulCodando <160548310+RaulCodando@users.noreply.github.com>
2024-09-28 17:17:06 -0300

    Create prim.cpp

---
+++ Arquivos em C++/prim.cpp
@@ -0,0 +1,125 @@
+#include "WeightedGraph.h"
+#include <climits>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+typedef struct output{
+    int cost;
+    vector<pair<int, int>> edges;
+}Output;
+
+Output prim(WeightedGraph g, int v0){
+    int n = g.getN();
+    using vertex = pair<int, int>;
+
+    vector<vector<vertex>> adjList = g.getAdjList();
+    priority_queue<vertex, vector<vertex>, greater<vertex>> f;
+    vector<int> parent(n, -1);
+    vector<int> cost(n, INT_MAX);
+    vector<bool> visited(n, false);
+
+    cost[v0-1] = 0;
+    f.push({0, v0-1});
+
+    while(!f.empty()){
+        vertex v = f.top();
+        f.pop();
+
+        visited[v.second] = true;
+
+        for(auto u: adjList[v.second]){
+            if(!visited[u.second] && u.first < cost[u.second]){
+                parent[u.second] = v.second;
+                cost[u.second] = u.first;
+                f.push(u);
+            }
+        }
+    }
+
+    int sum = 0;
+    vector<vertex> edges;
+
+    for(int i = 1; i < n; i++){
+        sum += cost[i];
+        edges.push_back({parent[i]+1, i+1});
+    }
+
+    edges.shrink_to_fit();
+
+    Output result;
+    result.cost = sum;
+    result.edges = edges;
+
+    return result;
+}
+
+void help(){
+    cout << "-h : mostra o help" << endl;
+    cout << "-o <arquivo> : redireciona a saida para o ‘‘arquivo’’" << endl;
+    cout << "-f <arquivo> : indica o ‘‘arquivo’’ que contém o grafo de entrada" << endl;
+    cout << "-s : mostra a solução" << endl;
+    cout << "-i : vértice inicial (para o algoritmo de Prim)" << endl;
+}
+
+int main(int argc, char *argv[]){
+    int v0;
+    string inputFile;
+    bool show = false;
+
+    for(int i = 1; i < argc; i++){
+        string arg = argv[i];
+
+        if(arg == "-i" && i + 1 < argc){
+            v0 = stoi(argv[++i]);
+        }
+        else if(arg == "-s"){
+            show = true;
+        }
+        else if(arg == "-f" && i + 1 < argc){
+            inputFile = argv[++i];
+        }
+        else if(arg == "-h"){
+            help();
+        }
+        else{
+            cerr << "Parâmetro desconhecido ou faltando argumento: " << arg << endl;
+            return 1;
+        }
+    }
+
+    ifstream file(inputFile);
+
+    if(!file.is_open()){
+        cerr << "Erro: não foi possível abrir o arquivo " << inputFile << "!\n";
+        return 1;
+    }
+
+    int n, m;
+
+    file >> n >> m;
+
+    WeightedGraph g(n, false);
+
+    for(int i = 0; i < m; i++){
+        int v1, v2, weight;
+
+        file >> v1 >> v2 >> weight;
+
+        g.addEdge(v1, v2, weight);
+    }
+
+    Output result = prim(g, v0);
+
+    if(show){
+        vector<pair<int, int>> edges = result.edges;
+
+        for(int i = 0; i < edges.size(); i++){
+            cout << "(" << edges[i].first << "," << edges[i].second << ") ";
+        }
+    }
+    else cout << result.cost;
+
+    return 0;
+}
